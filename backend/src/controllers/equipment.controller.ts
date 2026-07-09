0b2e53a576b4b7f69c363896ddf955aa1a505753
saki81 <cike198110@hotmail.com>
2026-07-09 14:21:44 +0200

    backend: add logic controller createEquipment and getEquipments

--- backend/src/controllers/equipment.controller.ts
+++ backend/src/controllers/equipment.controller.ts
@@ -3,11 +3,196 @@ import { prisma } from "../lib/prisma.js";
 
 
 export const createEquipment = async (req:Request, res: Response) => {
+    try {
 
+        const userId = req.user?.userId;
+
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        // Active gym
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                activeGymId: true
+            }
+        });
+
+        if (!user?.activeGymId) {
+            return res.status(400).json({ message: "No active gym selected"})
+        }
+
+        const gymId = user.activeGymId;
+
+        // OWNER STAFF OR GLOBAL ADMIN
+        const access = await prisma.admin.findFirst({
+            where: {
+                userId,
+                OR: [
+                    {
+                       gymId
+                    },
+                    {
+                       role: "ADMIN",
+                       gymId: null
+                    }
+                ]
+            }
+        });
+
+        if (!access) {
+            return res.status(403).json({ message: "You dont have permission"})
+        }
+
+        const {
+            name,
+            brand,
+            model,
+            inventoryNumber,
+            serialNumber,
+            serviceIntervalDays,
+            categoryId
+        } = req.body;
+
+        // Category must belong to the same gym
+        const category = await prisma.category.findFirst({
+            where: {
+                id: categoryId,
+                gymId
+            }
+        });
+
+        if (!category) {
+            return res.status(404).json({ message: "Category not found "});
+        }
+
+        // Inventory number unique to gym
+        if (inventoryNumber) {
+
+            const existingEquipment  = await prisma.equipment.findFirst({
+                where: {
+                    gymId,
+                    inventoryNumber
+                }
+            });
+
+            if (existingEquipment) {
+                return res.status(409).json({ message: "Inventory number already exists"});
+            }
+        }
+
+        const equipment = await prisma.equipment.create({
+             
+            data: {
+                name,
+                brand,
+                model,
+                inventoryNumber,
+                serialNumber,
+                serviceIntervalDays,
+                categoryId,
+
+                gymId,
+                userId
+            },
+            
+            include: {
+                category: true
+            }
+        });
+
+        return res.status(201).json({ message: "Equipment created successfully", equipment})
+        
+    } catch (error: any) {
+        return res.status(500).json({ message: error.message})
+    }
 };
 
 export const getEquipments = async (req:Request, res:Response) => {
+    try {
 
+        const userId = req.user?.userId;
+
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized"})
+        }
+
+        const user = await prisma.user.findUnique({
+
+            where: {
+                id: userId
+            },
+
+            select: {
+                activeGymId: true
+            }
+        });
+
+        if (!user?.activeGymId) {
+            return res.status(400).json({ message: "No active gym selected" })
+        }
+
+        const gymId = user.activeGymId;
+
+        const access = await prisma.admin.findFirst({
+
+            where: {
+                userId,
+                OR: [
+                    {
+                        gymId
+                    },
+                    {
+                        role: "ADMIN",
+                        gymId: null
+                    }
+                ]
+            }
+        });
+
+        if (!access) {
+            return res.status(403).json({ message: "Access denied"})
+        }
+
+        const equipments = await prisma.equipment.findMany({
+
+            where: {
+                gymId
+            },
+
+            include: {
+
+                category: true,
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                },
+            _count: {
+                    select: {
+                        maintenances: true
+                    }
+                }
+
+            },
+
+            orderBy: {
+                createdAt: "desc"
+            }
+
+        });
+
+        return res.status(200).json({ equipments });
+
+       
+    } catch (error: any) {
+        return res.status(500).json({ message: error.message})
+    }
 };
 
 export const getEquipmentById = async (req:Request, res:Response) => {
