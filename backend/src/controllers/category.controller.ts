fa9a6e01aabf726ec59618265801341ca5e21e97
saki81 <cike198110@hotmail.com>
2026-07-14 14:38:02 +0200

    backend: add createCategory logic

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -4,11 +4,51 @@ import { getGymAccess } from "../utils/gymAccess.js";
 
 export const createCategory = async ( req: Request, res: Response) => {
   try {
+    const userId = req.user?.userId;
 
-  } catch (error) {
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const gymAccess = await getGymAccess(userId, {
+      minimumRole: "STAFF"
+    });
+
+    if (!gymAccess) {
+      return res.status(403).json({ message: "Access denied" })
+    };
+
+    const { gymId } = gymAccess;
+
+    const { name } = req.body;
+
+    // Category name must be unique within the gym
+    const existingCategory = await prisma.category.findUnique({
+       where: {
+        gymId_name: {
+          gymId,
+          name,
+        },
+      },
+    });
+
+    if (existingCategory) {
+      return res.status(409).json({ message: "Category already exists"})
+    }
+
+    const category = await prisma.category.create({
+        data: {
+          name,
+          gymId
+        },
+    });
+
+    return res.status(201).json({ message: "Category created successfully", category })
+
+  } catch (error: any) {
     console.error(error);
     res.status(500).json({
-      message: "Internal server error",
+      message: error.message,
     });
   }
 };
