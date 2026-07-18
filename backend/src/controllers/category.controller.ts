b1f36eac68c57dc281710c2ec93d86e0ce133ac8
saki81 <cike198110@hotmail.com>
2026-07-18 14:24:54 +0200

    backend: add logic controller updateCategory and deleteCategory

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -101,22 +101,143 @@ export const getCategories = async (req: Request, res: Response) => {
 
 export const updateCategory = async (req: Request, res: Response) => {
   try {
+    const userId = req.user?.userId;
 
-  } catch (error) {
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized"})
+    }
+    
+    const gymAccess = await getGymAccess(userId, {
+      minimumRole: "STAFF",
+    });
+
+    if (!gymAccess) {
+      return res.status(403).json({ message: "Access denied"})
+    }
+
+    const { gymId } = gymAccess;
+
+    const { categoryId } = req.params;
+    const { name } = req.body;
+
+    const category = await prisma.category.findFirst({
+      where: {
+        id: categoryId,
+        gymId,
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    // Check if category exists
+    const existingCategory = await prisma.category.findUnique({
+      where: {
+         gymId_name: {
+           gymId,
+           name
+         },
+      }, 
+    });
+
+    if (existingCategory && existingCategory.id !== category.id) {
+      return res.status(409).json({ messsage: "Category already exists"});
+    }
+
+    const updateCategory = await prisma.category.update({
+      where: {
+         id: category.id,
+      }, 
+      data: {
+         name,
+      },
+    });
+
+    return res.status(200).json({ 
+      message: "Category updated successfully",
+      category: updateCategory,
+    })
+  } catch (error: any) {
     console.error(error);
     res.status(500).json({
-      message: "Internal server error",
+      message: error.message,
     });
   }
 };
 
 export const deleteCategory = async (req: Request, res: Response) => {
   try {
+     const userId = req.user?.userId;
 
-  } catch (error) {
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const gymAccess = await getGymAccess(userId, {
+      minimumRole: "STAFF",
+    });
+
+    if (!gymAccess) {
+      return res.status(403).json({
+        message: "Access denied",
+      });
+    }
+
+    const { gymId } = gymAccess;
+
+    const { categoryId } = req.params;
+
+    const category = await prisma.category.findFirst({
+      where: {
+        id: categoryId,
+        gymId,
+      },
+      include: {
+        equipments: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({
+        message: "Category not found",
+      });
+    }
+
+    
+    if (category.isDefault) {
+      return res.status(400).json({
+        message: "Default categories cannot be deleted",
+      });
+    }
+
+   
+    if (category.equipments.length > 0) {
+      return res.status(400).json({
+        message: "Cannot delete category because it contains equipment",
+      });
+    }
+
+    await prisma.category.delete({
+      where: {
+        id: category.id,
+      },
+    });
+
+    return res.status(200).json({
+      message: "Category deleted successfully",
+    });
+
+  } catch (error: any) {
     console.error(error);
     res.status(500).json({
-      message: "Internal server error",
+      message: error.message,
     });
   }
 };
