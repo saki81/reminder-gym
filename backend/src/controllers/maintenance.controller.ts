741cd7122ea01c91d28e16a846e74fbc482bdc37
saki81 <cike198110@hotmail.com>
2026-07-26 20:02:42 +0200

    backend: add logic createMaintenance controller

--- backend/src/controllers/maintenance.controller.ts
+++ backend/src/controllers/maintenance.controller.ts
@@ -2,11 +2,73 @@ import { Request, Response } from "express";
 import { prisma } from "../lib/prisma.js";
 import { getGymAccess } from "../utils/gymAccess.js";
 
-export const createMaintenance = async (
-  req: Request,
-  res: Response
-) => {
+export const createMaintenance = async (req: Request, res: Response) => {
   try {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" })
+    }
+
+    const gymAccess = await getGymAccess(userId, {
+      minimumRole: "STAFF"
+    });
+
+    if (!gymAccess) {
+      return res.status(403).json({ message: "Access denied"})
+    }
+
+    const { gymId } = gymAccess;
+
+    const {
+      title,
+      description,
+      cost,
+      status,
+      performedAt,
+      nextDueDate,
+      equipmentId
+    } = req.body;
+
+    const equipment = await prisma.equipment.findFirst({
+      where: {
+         id: equipmentId,
+         gymId
+      },
+    });
+
+    if (!equipment) {
+      return res.status(404).json({ message: "Equipment not found" })
+    }
+
+    const maintenance = await prisma.maintenance.create({
+      data: {
+        title,
+        description,
+        cost,
+        status,
+        performedAt,
+        nextDueDate,
+        equipmentId,
+
+        gymId,
+        userId,
+      },
+      include: {
+        equipment: {
+          select: {
+            id: true,
+            name: true,
+            inventoryNumber: true
+          }
+        }
+      }
+    });
+
+    return res.status(201).json({ 
+       message: "Maintenance created successfully",
+       maintenance,
+      });
 
   } catch (error: any) {
     return res.status(500).json({
