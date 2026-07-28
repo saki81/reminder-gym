f7313cd0fd90892872271ca434070ac1f0ff9088
saki81 <cike198110@hotmail.com>
2026-07-28 14:19:41 +0200

    backend: add basic controller logic getMaintenances

--- backend/src/controllers/maintenance.controller.ts
+++ backend/src/controllers/maintenance.controller.ts
@@ -77,11 +77,72 @@ export const createMaintenance = async (req: Request, res: Response) => {
   }
 };
 
-export const getMaintenances = async (
-  req: Request,
-  res: Response
-) => {
+export const getMaintenances = async (req: Request, res: Response) => {
   try {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized"});
+    }
+
+    const gymAccess = await getGymAccess(userId, {
+      minimumRole: "STAFF",
+    });
+
+    if (!gymAccess) {
+      return res.status(403).json({ message: "Access denied"});
+    }
+
+    const { gymId } = gymAccess;
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 12;
+
+    const skip = (page - 1) * limit;
+
+    const [maintenances, total] = await Promise.all([
+      prisma.maintenance.findMany({
+        where: {
+          gymId,
+        },
+        include: {
+          equipment: {
+            select: {
+              id: true,
+              name: true,
+              inventoryNumber: true,
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+        orderBy: {
+          performedAt: "desc"
+        },
+        skip,
+        take: limit,
+      }),
+
+      prisma.maintenance.count({
+        where: {
+          gymId,
+        },
+      }),
+    ]);
+
+    return res.status(200).json({
+       maintenances,
+       pagination: {
+         total,
+         page,
+         limit,
+         totalPages: Math.ceil(total / limit)
+       }
+    })
 
   } catch (error: any) {
     return res.status(500).json({
